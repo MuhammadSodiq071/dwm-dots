6a67ab409dffbd61f66a186fc3e6e665192c2d92
sda1 void muhammadsodiq <musozonovmuhammadsodik@gmail.com>
2025-03-02 15:18:29 +0500

    update

--- build/install.sh
+++ build/install.sh
@@ -1,47 +1,46 @@
-# doas setup
-echo 'Enter the root password'
+echo 'doas setup... Enter the root password'
 su -c "xbps-install opendoas && touch /etc/doas.conf && echo 'permit persist :wheel' > /etc/doas.conf"
 
-# installing packages
+echo "Installing packages"
 doas xbps-install -Su
 doas xbps-install dunst libnotify fzf lsd kitty fastfetch clipmenu flameshot brightnessctl firefox man dmenu rofi xcape setxkbmap feh xorg zsh gtk-engine-murrine neovim pnpm xdg-user-dirs btop mesa-vulkan-radeon ranger bat flatpak mpv imv vlc lxappearance qt{5,6}ct tty-clock libX{11,inerama,ft,randr}-devel imlib2-devel curl gpick translate-shell xclip xcursor-themes picom pulseaudio alsa-plugins-pulseaudio alsa-utils
 
-# installing Gruvbox-Material theme
+echo "Copying dwm.desktop for display manager"
+doas mkdir -p /usr/share/xsessions
+doas cp ~/dwm-dots/build/dwm.desktop /usr/share/xsessions/
+
+echo "Installing Gruvbox-Material theme"
 git clone https://github.com/TheGreatMcPain/gruvbox-material-gtk.git
 doas cp -r ~/gruvbox-material-gtk/themes/Gruvbox-Material-Dark /usr/share/themes
 doas cp -r ~/gruvbox-material-gtk/icons/Gruvbox-Material-Dark /usr/share/icons
+doas rm -rf gruvbox-material-gtk
 
-# copying configs
-cp -r ~/dwm-dots/wallpaper ~/
+echo "Copying configs..."
+mkdir ~/wallpaper
+ln ~/dwm-dots/wallpaper/* ~/wallpaper/
 mkdir ~/.config
 cp -r ~/dwm-dots/.config/* ~/.config/
 mkdir ~/.local
 cp -r ~/dwm-dots/.local/* ~/.local/
-cp ~/dwm-dots/.Xresources ~/
-cp ~/dwm-dots/.xprofile ~/
-cp ~/dwm-dots/.gtkrc-2.0 ~/
+ln ~/dwm-dots/.Xresources ~/
+ln ~/dwm-dots/.xprofile ~/
+ln ~/dwm-dots/.gtkrc-2.0 ~/
 
-# compiling dwm, dwmblocks(bar) and slock(lockscreen)
+echo "Compiling dwm, dwmblocks(bar) and slock(lockscreen)"
 (cd ~/.local/src/dwm && make && doas make install)
 (cd ~/.local/src/dwmblocks && make && doas make install)
 (cd ~/.local/src/slock && make && doas make install)
 
-# environment setup
+echo "Configuring Environment..."
 echo "QT_QPA_PLATFORMTHEME=qt6ct" | doas tee -a /etc/environment
 echo "GTK_THEME=Gruvbox-Material-Dark" | doas tee -a /etc/environment
 echo "PATH=/home/$USER/.local/bin" | doas tee -a /etc/environment
 
-# for touchpad setup
+echo "Touchpad setup..."
 doas mkdir -p /etc/X11/xorg.conf.d
 doas ln /usr/share/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d
 
-# zsh setup
-echo "/sbin/zsh" | doas tee -a /etc/shells
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/MichaelAquilina/zsh-auto-notify.git $ZSH_CUSTOM/plugins/auto-notify
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp ~/dwm-dots/.zshrc ~/
+echo "ZSH setup"
+~/dwm-dots/build/zsh.sh
+ln ~/dwm-dots/.zshrc ~/
 

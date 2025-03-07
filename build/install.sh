6913915a73902eaa921d0540e7d3bcaca6505d83
muhammadsodiq sda2 arch <musozonovmuhammadsodik@gmail.com>
2025-03-07 14:42:09 +0000

    update

--- build/install.sh
+++ build/install.sh
@@ -1,5 +1,5 @@
 echo "Installing packages"
-sudo pacman -S dunst libnotify fzf lsd kitty fastfetch clipmenu flameshot brightnessctl firefox pipewire pipewire-alsa pipewire-pulse opendoas man dmenu rofi xcape xorg-setxkbmap feh xorg telegram-desktop unzip zsh gtk-engine-murrine neovim npm xdg-user-dirs btop vulkan-radeon ranger bat gtk2 gtk3 gtk4 flatpak bluez bluez-utils blueman openssh ssh-tools amd-ucode mpv imv vlc lxappearance qt5ct qt6ct picom
+sudo pacman -S dunst libnotify fzf lsd alacritty ueberzugpp fastfetch clipmenu flameshot brightnessctl firefox pipewire pipewire-alsa pipewire-pulse opendoas man dmenu rofi xcape xorg-setxkbmap feh xorg telegram-desktop unzip zsh gtk-engine-murrine neovim npm xdg-user-dirs btop vulkan-radeon ranger bat gtk2 gtk3 gtk4 flatpak bluez bluez-utils blueman openssh ssh-tools amd-ucode mpv imv vlc lxappearance qt5ct qt6ct picom
 
 echo "Installing paru and aur packages"
 git clone https://aur.archlinux.org/paru-bin.git && (cd paru-bin && makepkg -si) && rm -rf paru-bin
@@ -13,21 +13,24 @@ doas cp ~/dwm-dots/build/dwm.desktop /usr/share/xsessions/
 echo "Enabling PipeWire"
 systemctl --user enable --now pipewire pipewire-pulse
 
-echo "Installing Gruvbox-Material theme"
+echo "Installing Gruvbox-Material theme and JetBrainsMono Nerd Fonts"
 git clone https://github.com/TheGreatMcPain/gruvbox-material-gtk.git
 doas cp -r ~/gruvbox-material-gtk/themes/Gruvbox-Material-Dark /usr/share/themes
 doas cp -r ~/gruvbox-material-gtk/icons/Gruvbox-Material-Dark /usr/share/icons
 rm -rf gruvbox-material-gtk
 
+doas mkdir -p /usr/share/fonts/nerdfonts
+doas cp ~/dwm-dots/build/*.ttf /usr/share/fonts/nerdfonts
+
 echo "Copying configs"
-mkdir ~/wallpaper && ln ~/dwm-dots/wallpaper/* ~/wallpaper
 mkdir ~/.config
 cp -r ~/dwm-dots/.config/* ~/.config/
 mkdir ~/.local
 cp -r ~/dwm-dots/.local/* ~/.local/
-ln ~/dwm-dots/.Xresources ~/
-ln ~/dwm-dots/.xprofile ~/
-ln ~/dwm-dots/.gtkrc-2.0 ~/
+cp ~/dwm-dots/.Xresources ~/
+cp ~/dwm-dots/.xprofile ~/
+cp ~/dwm-dots/.gtkrc-2.0 ~/
+cp -r ~/dwm-dots/wallpaper ~/
 
 echo "Compiling dwm, dwmblocks(bar) and slock(lockscreen)"
 (cd ~/.local/src/dwm/ && make && sudo make install)
@@ -43,4 +46,4 @@ echo "PATH=$HOME/.local/bin" | doas tee -a /etc/environment
 
 echo "Setup ZSH"
 ~/dwm-dots/build/zsh
-ln ~/dwm-dots/.zshrc ~/
+cp ~/dwm-dots/.zshrc ~/

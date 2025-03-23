3a69677d5dddc99faccf899ea323b8844d5bc5c0
sda1 void muhammadsodiq <musozonovmuhammadsodik@gmail.com>
2025-03-23 15:11:01 +0500

    update

---
+++ prefs.js
@@ -0,0 +1,23 @@
+// Mozilla User Preferences
+
+user_pref("breakpad.reportURL", "");
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.sessionstore.enabled", false);
+user_pref("browser.sessionstore.interval", 600000);
+user_pref("browser.sessionstore.max_resumed_crashes", 0);
+user_pref("browser.sessionstore.max_tabs_undo", 0);
+user_pref("browser.sessionstore.max_windows_undo", 0);
+user_pref("browser.sessionstore.restore_tabs_lazily", false);
+user_pref("browser.sessionstore.resume_from_crash", false);
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("datareporting.healthreport.infoURL", "");
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("datareporting.policy.firstRunURL", "");
+user_pref("extensions.pocket.api", "");
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.pocket.oAuthConsumerKeyBff", "");
+user_pref("extensions.pocket.site", "");
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("privacy.trackingprotection.enabled", true);
+
